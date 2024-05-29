896cec33f9c55df70515bd326192596e3c8474ba
ankur soni <an2112soni@gmail.com>
2024-05-29 23:43:48 +0530

    minor change #3

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -18,10 +18,17 @@ function HomePage() {
     authService
       .getCurrentUser()
       .then((response) => {
-        navigate("/home/1");
-        console.log("App :: useEffect :: response", response);
-        dispatch(login(response));
-        setLoading(false);
+        if (response === null) {
+          navigate("/login");
+          console.log("App :: useEffect :: response", response);
+          setLoading(false);
+          return;
+        } else {
+          navigate("/home/1");
+          console.log("App :: useEffect :: response", response);
+          dispatch(login(response));
+          setLoading(false);
+        }
       })
       .catch((error) => {
         navigate("/login");
@@ -36,7 +43,7 @@ function HomePage() {
       <MainContainer />
     </Container>
   ) : (
-    <Container className={'flex-col justify-center h-full'}>
+    <Container className={"flex-col justify-center h-full"}>
       <span className="flex items-center justify-center gap-2 h-full">
         <img className="h-32 w-32 animate-spin" src={spinner} alt="loading" />
       </span>
